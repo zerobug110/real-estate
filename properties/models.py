b39fddff224c69b73256714fe8a41362a2bf8620
zerobug110 <mahabojang110@gmail.com>
2021-04-30 02:29:56 +0000

    added database

--- properties/models.py
+++ properties/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
 
-class properties(model.Model):
-    name = model.models.CharField(max_length=50),
-    price = model.models.FloatField(),
-    stock = model.models.IntegerField(),
-    image_url = model.models.CharField(max_length=2083)
+class Properties(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    size = models.CharField(max_length=255)
+    image_url = models.CharField(max_length=2083)
+
+
