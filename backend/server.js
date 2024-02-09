c5dcd3594a26c61205fdd9b420b33285558fa7ff
Muhammed Bojang <mahabojang110@gmail.com>
2024-02-09 10:50:21 +0000

    imported the server files

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,3 @@
-
-// const express = require('express')
-// const http = require('http')
-// const app = require('./src/app/app')
-// const colors = require('./src/node_modules/colors')
-
-// const server = http.createServer(app)
-// const PORT = process.env.PORT || 5000
-
-// server.listen(PORT, () => {
-//     console.log(`******************* server running on port ${PORT} ***********************` .yellow)
-// })
-
-require("dotenv").config()
 require("dotenv").config()
 const http = require("http")
 const colors = require("colors")
